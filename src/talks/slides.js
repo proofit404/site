5900c575e5c18c3bdc0a279b71df7d41f33193c9
Artem Malyshev <proofit404@gmail.com>
2021-02-14 03:38:01 +0300

    Setup reveal.js framework.

---
+++ src/talks/slides.js
@@ -0,0 +1,13 @@
+import hljs from "highlight.js";
+import Reveal from "reveal.js/dist/reveal";
+
+hljs.initHighlightingOnLoad();
+
+Reveal.initialize({
+  controls: false,
+  progress: false,
+  slideNumber: true,
+  history: true,
+  center: true,
+  transition: "none"
+});
